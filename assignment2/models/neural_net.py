9c929ec6bd371d07188fd8c153df24fabac929ea
kie4280 <kie4280@livemail.tw>
2022-02-27 12:25:27 -0600

    finished backwards but failed the gradient check

--- assignment2/models/neural_net.py
+++ assignment2/models/neural_net.py
@@ -1,6 +1,6 @@
 """Neural network model."""
 
-from typing import Sequence
+from typing import Dict, Sequence
 
 import numpy as np
 
@@ -65,6 +65,14 @@ class NeuralNetwork:
 
         return np.matmul(X, W) + b
 
+    def weight_grad(self, W: np.ndarray, X: np.ndarray) -> np.ndarray:
+        output = np.zeros((X.shape[0], W.shape[1], W.shape[0], W.shape[1]))
+        for i in range(X.shape[0]):
+            for j in range(W.shape[1]):
+                output[i, j, :, j] = X[i, :]
+
+        return output
+
     def relu(self, X: np.ndarray) -> np.ndarray:
         """Rectified Linear Unit (ReLU).
         Parameters:
@@ -84,7 +92,9 @@ class NeuralNetwork:
             the output data
         """
         # TODO: implement me
-        diag = np.diagflat(np.where(X > 0, 1, 0))
+        diag = np.zeros((X.shape[0], X.shape[1], X.shape[1]))
+        for i in range(X.shape[0]):
+            diag[i, :, :] = np.diagflat(np.where(X[i] > 0, 1, 0))
         return diag
 
     def softmax(self, X: np.ndarray) -> np.ndarray:
@@ -97,17 +107,20 @@ class NeuralNetwork:
         # TODO: implement me
         X = X - np.max(X, axis=1, keepdims=True)
         cate = np.exp(X)
-        return cate / np.sum(cate, axis=1)
+        return cate / np.sum(cate, axis=1, keepdims=True)
 
-
-    
     def softmax_grad(self, X: np.ndarray) -> np.ndarray:
         """
         unsure
+        Compute the gradient over all samples
         """
-        X = np.expand_dims(X, -1)
-        output = np.matmul(X, -X.T)
-        output = output + np.diagflat(X)
+        output = np.zeros(
+            (X.shape[0], X.shape[1], X.shape[1]), dtype=np.float64)
+        for i in range(X.shape[0]):
+            s = X[i]
+            s = np.expand_dims(s, -1)
+            y = np.matmul(s, -s.T)
+            output[i, :, :] = y + np.diagflat(s)
 
         return output
 
@@ -121,16 +134,15 @@ class NeuralNetwork:
             Matrix of shape (N, C) where scores[i, c] is the score for class
                 c on input X[i] outputted from the last layer of your network
         """
-        self.outputs = {}
+        self.outputs: Dict[str, np.ndarray] = {}
         # TODO: implement me. You'll want to store the output of each layer in
         # self.outputs as it will be used during back-propagation. You can use
         # the same keys as self.params. You can use functions like
         # self.linear, self.relu, and self.softmax in here.
 
-        W: np.ndarray = self.params["W" + str(1)]
-        b: np.ndarray = self.params["b" + str(1)]
-        self.linear(W, X, b)
-        for layer in range(2, self.num_layers + 1):
+        self.outputs[str(0)] = X
+
+        for layer in range(1, self.num_layers + 1):
             W: np.ndarray = self.params["W" + str(layer)]
             b: np.ndarray = self.params["b" + str(layer)]
             X = self.linear(W, X, b)
@@ -152,17 +164,75 @@ class NeuralNetwork:
         Returns:
             Total loss for this batch of training samples
         """
-        self.gradients = {}
+        self.gradients: Dict[str, np.ndarray] = {}
         # TODO: implement me. You'll want to store the gradient of each
         # parameter in self.gradients as it will be used when updating each
         # parameter and during numerical gradient checks. You can use the same
         # keys as self.params. You can add functions like self.linear_grad,
         # self.relu_grad, and self.softmax_grad if it helps organize your code.
+        loss = -np.log(self.outputs[str(self.num_layers)]
+                       [range(y.shape[0]), y])
+        # regularization
+        loss = loss + reg * np.sum(
+            np.array([np.sum(self.params["W" + str(i+1)] ** 2) for i in range(self.num_layers)]))
+
+        loss = loss / y.shape[0]
+
+        activ = self.outputs[str(self.num_layers)]
+        ll_W = self.params["W" + str(self.num_layers)]
+        ll_b = self.params["b" + str(self.num_layers)]
+
+        grad_CEL = np.zeros((y.shape[0], 1, activ.shape[1]))
+        grad_CEL[range(y.shape[0]), 0, y] = -1 / \
+            activ[range(y.shape[0]), y]  # unsure
+        activ_grad = self.softmax_grad(activ)  # unsure
+        grad_W = self.weight_grad(ll_W, self.outputs[str(self.num_layers-1)])
+        grad_x = ll_W.T
+
+        ba = np.matmul(grad_CEL, activ_grad)
+        # print(ll_W.shape, grad_CEL.shape, activ_grad.shape, grad_W.shape)
+        # print(g.shape)
+        gw = np.zeros((y.shape[0], 1, grad_W.shape[2], grad_W.shape[3]))
+
+        for i in range(y.shape[0]):
+            # print(g[i].shape, grad_W.shape)
+            gw[i] = np.tensordot(ba[i], grad_W[i], axes=1)
+        gw = gw.squeeze(axis=1)
+        
+        grad_upstream = np.matmul(ba, grad_x)
+        grad_b = np.identity(ba.shape[2]) # unsure, probably need a identiy matrix?
+        self.gradients["W" + str(self.num_layers)] = gw
+        self.gradients["b" + str(self.num_layers)] = np.matmul(ba, grad_b)
 
 
 
+        for layer in range(self.num_layers-1, 0, -1):
 
-        return
+            activ = self.outputs[str(layer)]
+            ll_W = self.params["W" + str(layer)]
+            ll_b = self.params["b" + str(layer)]
+           
+            activ_grad = self.relu_grad(activ)  # unsure
+            grad_W = self.weight_grad(ll_W, self.outputs[str(layer-1)])
+            grad_x = ll_W.T
+
+            ba = np.matmul(grad_upstream, activ_grad)
+            # print(ll_W.shape, grad_CEL.shape, activ_grad.shape, grad_W.shape)
+            # print(g.shape)
+            gw = np.zeros((y.shape[0], 1, grad_W.shape[2], grad_W.shape[3]))
+
+            for i in range(y.shape[0]):
+                # print(g[i].shape, grad_W.shape)
+                gw[i] = np.tensordot(ba[i], grad_W[i], axes=1)
+            gw = gw.squeeze(axis=1)
+            
+            grad_upstream = np.matmul(ba, grad_x)
+            grad_b = np.identity(ba.shape[2]) # unsure, probably need a identiy matrix?
+            self.gradients["W" + str(layer)] = gw
+            self.gradients["b" + str(layer)] = np.matmul(ba, grad_b)
+
+
+        return np.sum(loss)
 
     def update(
         self,
@@ -185,6 +255,14 @@ class NeuralNetwork:
         # handle updates for both SGD and Adam depending on the value of opt.
 
         if opt == 'SGD':
+            for i in range(self.num_layers):
+                W: np.ndarray = self.params["W" + str(i+1)]
+                b: np.ndarray = self.params["b" + str(i+1)]
+                self.params["W" + str(i+1)] = W - lr * \
+                    self.gradients["W" + str(i+1)]
+                self.params["b" + str(i+1)] = b - lr * \
+                    self.gradients["b" + str(i+1)]
+
             pass
 
         elif opt == 'Adam':
